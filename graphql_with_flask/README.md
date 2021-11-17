c9d26e7a4bb096798e790d98ac3fc8949d40319a
Yosef Salmalian <flaslnm@gmail.com>
2021-11-17 14:57:58 +0330

    sqlalchemy database migrations

--- graphql_with_flask/README.md
+++ graphql_with_flask/README.md
@@ -14,6 +14,16 @@ $ pip install -r requirements.txt
 ```
 
 
-# Resources
-Here are some good articles and documentations that I found useful:
-* [Using migrations for sqlalchemy](https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/)
+# Resources and Notes
+Here are some good articles and documentations that I found useful:  
+* [How to create a one to many relation](https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/#simple-relationships)
+* [Using migrations for sqlalchemy](https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/)  
+* [Using multi choice in sqlalchemy](https://sqlalchemy-utils.readthedocs.io/en/latest/data_types.html#module-sqlalchemy_utils.types.choice)
+
+And Here are some usefull snippets:
+* Initation of migrations using flask cli:
+```
+$ flask db init # initate migrations
+$ flask db migrate # create migration files
+```
+
--- graphql_with_flask/tasks/models.py
+++ graphql_with_flask/tasks/models.py
@@ -1,4 +1,7 @@
+import enum
 from config import db
+from sqlalchemy_utils.types.choice import ChoiceType
+from users.models import User
 
 class Task(db.Model):
     __tablename__ = 'tasks'
@@ -7,6 +10,18 @@ class Task(db.Model):
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255), nullable=True)
 
+    user_id = db.Column(db.Integer, db.ForeignKey(User.__tablename__ + '.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref(__tablename__, lazy=True))
+
+    class TaskStatus(enum.Enum):
+        NOTSTARTED = 'NOTSTARTED'
+        INPROGRESS = 'INPROGRESS'
+        DONE = 'DONE'
+
+    status = db.Column(ChoiceType(TaskStatus, impl=db.String(16)), nullable=False, default=TaskStatus.NOTSTARTED)
+    due_date = db.Column(db.DateTime, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now(), onupdate=db.func.now())
 
     def __repr__(self):
         return '<Task %r>' % self.id
